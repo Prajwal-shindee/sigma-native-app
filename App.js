97fbe9b5fed5ea727a567e8f1a64c7f598dec1e6
Vishnu MD <vishnudivakaran1@gmail.com>
2023-06-24 16:44:40 +0530

    expo app with navigation

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginPanel from './components/LoginPanel';
-import RegistrationPanel from './components/RegistrationPanel';
-import Dashboard from './components/Dashboard';
-import ProfileUpdate from './components/ProfileUpdate';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginPanel from "./components/LoginPanel";
+import RegistrationPanel from "./components/RegistrationPanel";
+import Dashboard from "./components/Dashboard";
+import ProfileUpdate from "./components/ProfileUpdate";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginPanel} />
-        <Stack.Screen name="Registration" component={RegistrationPanel} />
-        <Stack.Screen name="Dashboard" component={Dashboard} />
-        <Stack.Screen name="ProfileUpdate" component={ProfileUpdate} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginPanel} />
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+          <Stack.Screen name="Register" component={RegistrationPanel} />
+          <Stack.Screen name="ProfileUpdate" component={ProfileUpdate} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 };
 
-export default App; 
+export default App;
--- components/LoginPanel.js
+++ components/LoginPanel.js
@@ -1,38 +1,83 @@
 // LoginPanel.js
 
-import React, { useState } from 'react';
-import { View, TextInput, Button, Alert } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  Button,
+  Alert,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
 
 const LoginPanel = () => {
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [password, setPassword] = useState('');
+  const [formData, setFormData] = useState({
+    phoneNumber: "",
+    password: "",
+  });
+
+  const { phoneNumber, password } = formData;
+
+  const navigation = useNavigation();
+
+  const handleChange = (value, name) => {
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
   const handleLogin = () => {
     // Perform API call to authenticate user's credentials
     // Replace the API call with your actual implementation
-    if (phoneNumber === '1234567890' && password === 'password') {
-      Alert.alert('Success', 'Login successful!');
+    if (phoneNumber === "admin" && password === "admin") {
+      Alert.alert("Success", "Login successful!");
+      navigation.navigate("Dashboard");
     } else {
-      Alert.alert('Error', 'Invalid phone number or password.');
+      Alert.alert("Error", "Invalid phone number or password.");
     }
   };
 
   return (
-    <View>
-      <TextInput
-        placeholder="Phone Number"
-        onChangeText={text => setPhoneNumber(text)}
-        value={phoneNumber}
-      />
-      <TextInput
-        placeholder="Password"
-        onChangeText={text => setPassword(text)}
-        value={password}
-        secureTextEntry
-      />
-      <Button title="Login" onPress={handleLogin} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <TextInput
+          placeholder="Phone Number"
+          onChangeText={(value) => handleChange(value, "phoneNumber")}
+          value={phoneNumber}
+        />
+        <TextInput
+          placeholder="Password"
+          name="password"
+          onChangeText={(value) => handleChange(value, "password")}
+          value={password}
+          secureTextEntry
+          style={styles.inputStyles}
+        />
+        <Button title="Login" onPress={handleLogin} />
+        <Button
+          title="Don't have an account? Register here"
+          onPress={() => navigation.navigate("Register")}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    width: "80%",
+    gap: 6,
+  },
+  inputStyles: {
+    marginTop: 10,
+  },
+});
 
 export default LoginPanel;
